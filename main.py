48423352b471af8ce4d97d1b699504d4b37a9ed6
yahiaislam2008 <yi820690@gmail.com>
2023-08-20 21:04:03 +0200

    1st

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #TODO Calculator FUNCTIONS
 
-
-
-
-
-
-def calculate(a, b, operation):
+def calculate(num1, num2, operation):
+        if operation == "*" or "x" or "X":
+                result=num1 * num2
+                return result
         #TODO (CONTINUE ON THE SAME PATTERN
 
 
 
 
-num1 = #TODO INPUT FROM USER
-num2 = #TODO INPUT FROM USER
-op = #TODO INPUT FROM USER
+num1 = int(input("Enter 1st number: "))#TODO INPUT FROM USER
+num2 = int(input("Enter 2nd number: "))#TODO INPUT FROM USER
+op = input("Enter your opration: ")#TODO INPUT FROM USER
 print(calculate(num1, num2, op))
